db1dba931bd16fddd99f38f4ab1c7617a723e89f
cdufour <opusidea@gmail.com>
2018-11-13 17:59:52 +0100

    J7 end

--- projet6/index.php
+++ projet6/index.php
@@ -5,10 +5,10 @@ try {
   var_dump($e);
 }
 
-
-$query = $pdo->prepare('SELECT count(*) FROM student');
+$query = $pdo->prepare('SELECT COUNT(*) FROM student');
 $query->execute();
 $nb_students = $query->fetch(PDO::FETCH_NUM)[0];
+
 $nb_students_per_page = 10;
 $nb_pages = ceil($nb_students / $nb_students_per_page);
 $offset = 0;
@@ -17,24 +17,14 @@ $students = [];
 if (isset($_GET['page'])) {
   $offset = (intval($_GET['page']) - 1) * $nb_students_per_page;
 
-  $query = $pdo->prepare(
-    'SELECT * FROM student LIMIT 10 OFFSET 10');
-
+  // $query = $pdo->prepare('SELECT * FROM student LIMIT :o , :l');
+  $query = $pdo->prepare('SELECT * FROM student LIMIT :l OFFSET :o');
+  $query->bindValue(':l', $nb_students_per_page, PDO::PARAM_INT);
+  $query->bindValue(':o', $offset, PDO::PARAM_INT);
   $query->execute();
 
-  // $query->execute([
-  //   ':limit' => $nb_students_per_page,
-  //   ':offset' => $offset
-  // ]);
-
   $students = $query->fetchAll(PDO::FETCH_OBJ);
-  var_dump($students);
-
 }
-
-
-
-
 
 ?>
 
